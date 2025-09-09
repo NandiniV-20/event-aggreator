083074360e3f80498a220c5b56e99e39f6f5e597
Nandini Dnyaneshwar Vaijawade <163897805+NandiniV-20@users.noreply.github.com>
2025-09-09 11:18:18 +0530

    Update db.py
    PostgreSQL database is used by replacing SQLite

--- db.py
+++ db.py
@@ -3,7 +3,9 @@ from sqlalchemy import create_engine, Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./events.db"
+import os
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -32,3 +34,4 @@ def get_db():
         yield db
     finally:
         db.close()
+
